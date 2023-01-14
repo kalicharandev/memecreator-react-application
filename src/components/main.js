15a6f713e5d2d082102c0a1658d28de8f8d20eff
unknown <meiltokalicharan@gmail.com>
2023-01-14 22:55:08 +0530

    to fix image

--- src/components/main.js
+++ src/components/main.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import memesData from '../memesData';
 export default function Main(){
-    let url;
+    const[memeImage, setMemeImage]= React.useState("");
     function getImage(){
             const memesArray = memesData.data.memes;
             const randomNumber = Math.floor(Math.random() * memesArray.length);
-             url = memesArray[randomNumber].url;
-           console.log(url);
+             setMemeImage(memesArray[randomNumber].url);
     }
+
     return(
         <main>
             <div className='form'>
                 <input  type="text"  className='form--input' placeholder='Top Text'/>
                 <input  type ="text" className='form--input' placeholder='Buttom Text'/>
                 <button onClick={getImage}  className='form--button'>Get a new meme image 🖼</button>
-            <p>{url}</p>
             </div>
-
+         <img src={memeImage} className='meme--image'/>
         </main>
     )
 }
